5b80f5be6c55b0711238a4d8edd8163fc729ccf2
Jan Halfar <jan@bestbytes.com>
2019-02-18 11:53:27 +0100

    added config file support and minimalistic html web interface

--- cmd/walker/walker.go
+++ cmd/walker/walker.go
@@ -11,18 +11,52 @@ import (
 	"os"
 	"strings"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/foomo/walker"
 	"github.com/foomo/walker/config"
+	yaml "gopkg.in/yaml.v1"
 )
 
 type server struct {
-	servicePath  string
-	serviveProxy http.Handler
-	p            *httputil.ReverseProxy
+	servicePath   string
+	serviveProxy  http.Handler
+	p             *httputil.ReverseProxy
+	s             *walker.Service
+	conf          string
+	reportHandler http.HandlerFunc
 }
 
+const pathReports = "/reports"
+
+const htmlIndex = `<html>
+<head><title>Walker</title></head>
+<body>
+	<h1>Walker</h1>
+	<ul>
+		<li><a href="/status">walker status</a></li>
+		<li><a href="/reports/results">all plain results (this can be a very long doc)</a></li>
+		<li><a href="/reports/list">list all jobs / results</a></li>
+		<li><a href="/reports/broken-links">broken links</a></li>
+		<li><a href="/reports/errors">errors</a></li>
+	</ul>
+</body>
+</html>`
+
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		w.Write([]byte(htmlIndex))
+		return
+	}
+	if r.URL.Path == "/status" {
+		w.Write([]byte(":::::::::::::::::: STATUS ::::::::::::::::::\n"))
+		w.Write([]byte("running with config:\n" + s.conf + "\n"))
+		w.Write([]byte("::::::::::::::::::::::::::::::::::::::::::::\n"))
+		s.s.Walker.PrintStatus(w, s.s.Walker.GetStatus())
+		return
+	}
+	if strings.HasPrefix(r.URL.Path, pathReports) {
+		s.reportHandler(w, r)
+		return
+	}
 	if strings.HasPrefix(r.URL.Path, s.servicePath) {
 		fmt.Println("service call", r.URL.Path)
 		s.serviveProxy.ServeHTTP(w, r)
@@ -40,7 +74,6 @@ func must(comment string, err error) {
 }
 
 func main() {
-
 	flag.Parse()
 	if len(flag.Args()) != 1 {
 		fmt.Println("usage:", os.Args[0], "path/to/config.yaml")
@@ -48,7 +81,13 @@ func main() {
 	}
 	conf, errConf := config.Get(flag.Arg(0))
 	must("config error:", errConf)
-	spew.Dump(conf)
+
+	yamlConfBytes, _ := yaml.Marshal(conf)
+	fmt.Println("this is how I understood your config:")
+	fmt.Println("------------------------------------------------------------------")
+	fmt.Println(string(yamlConfBytes))
+	fmt.Println("------------------------------------------------------------------")
+
 	proxyURL, errProxyURL := url.Parse(conf.Frontend)
 	must("can not parse frontend url: "+conf.Frontend+" :", errProxyURL)
 	fmt.Println("proxying frontend requests to", proxyURL)
@@ -64,8 +103,11 @@ func main() {
 	serviceProxy := walker.NewDefaultServiceGoTSRPCProxy(s, []string{})
 
 	log.Fatal(http.ListenAndServe(conf.Addr, &server{
-		servicePath:  serviceProxy.EndPoint,
-		serviveProxy: serviceProxy,
-		p:            httputil.NewSingleHostReverseProxy(proxyURL),
+		conf:          string(yamlConfBytes),
+		servicePath:   serviceProxy.EndPoint,
+		serviveProxy:  serviceProxy,
+		p:             httputil.NewSingleHostReverseProxy(proxyURL),
+		s:             s,
+		reportHandler: walker.GetReportHandler(pathReports, s.Walker),
 	}))
 }
