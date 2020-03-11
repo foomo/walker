df5259b447450df9c50d194ce51959075448cf52
Jan Halfar <jan@bestbytes.com>
2020-03-11 14:11:43 +0100

    added agent config

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"errors"
 	"io/ioutil"
+	"net/url"
 
 	yaml "gopkg.in/yaml.v1"
 )
@@ -28,14 +30,21 @@ type Config struct {
 	Paging            bool
 	IgnoreRobots      bool
 	GroupHeader       string
+	Agent             string
+}
+
+type shortConfig struct {
+	Target string
 }
 
 func Get(filename string) (conf *Config, err error) {
 	conf = &Config{
 		Concurrency:      2,
+		Addr:             ":3001",
 		UseCookies:       true,
 		IgnoreAllQueries: false,
 		IgnoreRobots:     false,
+		Agent:            "foomo-walker",
 	}
 	yamlBytes, errRead := ioutil.ReadFile(filename)
 	if errRead != nil {
@@ -46,6 +55,34 @@ func Get(filename string) (conf *Config, err error) {
 	if errUnmarshal != nil {
 		err = errUnmarshal
 		return
+	}
+	if conf.Target.BaseURL == "" {
+		// try to get a short one
+		shortConf := &shortConfig{}
+		errUnmarshal := yaml.Unmarshal(yamlBytes, &shortConf)
+		if errUnmarshal != nil {
+			err = errUnmarshal
+			return
+		}
+		if shortConf.Target != "" {
+			conf.Target.BaseURL = shortConf.Target
+		}
+	}
+	if len(conf.Target.Paths) == 0 {
+		baseURL, errURL := url.Parse(conf.Target.BaseURL)
+		if errURL != nil {
+			return nil, errors.New("target/target.baseurl can not be parsed: " + errURL.Error())
+		}
+		if baseURL.Path == "" {
+			conf.Target.Paths = []string{"/"}
+		} else {
+			conf.Target.Paths = []string{baseURL.Path}
+			baseURL.Path = ""
+			conf.Target.BaseURL = baseURL.String()
+		}
+	}
+	if conf.Target.BaseURL == "" {
+		return nil, errors.New("target base url must not be empty")
 	}
 	return
 }
